66eec015b3cfb14c5eff8bae81f7406d5b68583e
Colin Hale <Halecolin1@gmail.com>
2019-05-21 17:02:29 -0600

    Commit Quicksort-1-Partition solution

---
+++ Quicksort-1-Partition.js
@@ -0,0 +1,12 @@
+// Challenge located at https://www.hackerrank.com/challenges/quicksort1/problem\
+
+// Complete the quickSort function below.
+function quickSort(arr) {
+    return arr.sort((a,b) => a-b);
+}
+
+(()=>{
+    arr = [4,5,3,7,2];
+    // console.log('QuickSort Answer : ' , quickSort(arr));
+})()
+
