3e28dce128f75afbebc1ae388069d7faa5b14782
Colin Hale <Halecolin1@gmail.com>
2019-06-01 15:28:50 -0600

    adding jumpingOnCloud solution

---
+++ jumpingOnClouds.js
@@ -0,0 +1,16 @@
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c, k) {
+    let count = 100;
+    for(let i = 0; i < c.length; i+=k){
+        count --;
+        if(c[i] === 1){
+            count -= 2;
+        }
+    }
+    return count;
+}
+
+(()=>{
+    let c = [0,0,1,0,0,1,1,0], k = 2; 
+    console.log('jumpingOnClouds(c, k): ' , jumpingOnClouds(c, k))
+})()
