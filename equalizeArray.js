8c8296ca2f19053aca2c3139d7f7f6ca3ce3b614
colinah <halecolin1@gmail.com>
2020-01-25 18:09:04 -0700

    solved equalizeArray challenge

--- equalizeArray.js
+++ equalizeArray.js
@@ -1,11 +1,10 @@
 function equalizeArray(arr) {
     let a = [... new Set(arr)]
-    let b = new Array(a.length-1).fill(0);
+    let b = new Array(a.length).fill(0);
     arr.map(el => {
         b[a.indexOf(el)]++
     })
-    return b
-    
+    return arr.length - Math.max(...b)
     }
 let arr = [3,3,2,1,3]
     console.log(equalizeArray(arr))
