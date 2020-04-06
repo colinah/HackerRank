9d523afe72a683d4df54ae77358df39b3b6309d0
colinah <halecolin1@gmail.com>
2020-04-06 09:33:09 -0600

    Still some errors but done for day

--- appendAndDelete.js
+++ appendAndDelete.js
@@ -1,19 +1,42 @@
 // Complete the appendAndDelete function below.
 function appendAndDelete(s, t, k) {
-    let s = s.split('');
-    let t = t.split('');
-    let short = s
-    let long = t
+    if( s === t){
+        return 'Yes'
+    }
+    let short = s.split('');
+    let long = t.split('');
+    let index
+    let ans = 'No';
+    let check = true
 
     if(s.length > t.length){
-        short = t
-        long = s
+        short = t.split('');
+        long = s.split('');
     } 
 
     for (let i = 0; i < short.length; i++){
-
+        if( short[i] !== long[i]){
+            index = i
+            check = false
+            break;
+        }
     }
+    if(check){
+        index = short.length - 1;
+    }
+    let moves = (short.length - index) + (long.length - index);
+    console.log(moves, short.length, long.length, index)
+    if(moves === k){
+        ans = 'Yes';
+    }
+
+    return ans
 
 }
 
-()
+(()=>{
+    s = 'hackerhappy';
+    t = 'hackerrank';
+    k = 9;
+    console.log(appendAndDelete(s, t, k))
+    })()
