60aa65ac846b1b386869439110c4b804a974dd08
colinah <halecolin1@gmail.com>
2020-04-07 14:45:52 -0600

    Passes all tests but bigintigers

--- taumBday.js
+++ taumBday.js
@@ -2,7 +2,7 @@ function taumBday(b, w, bc, wc, z) {
     // Write your code here
     let minCost, maxCost, minCount, maxCount, total;
     bc > wc ? (maxCost = bc, minCost = wc, minCount = w, maxCount = b) : (maxCost = wc, minCost = bc, minCount = b, maxCount = w)
-    if ( minCost > maxCost - z ){
+    if ( minCost >= maxCost - z ){
         //If it is more costly to convert the materials
         console.log('No convert')
         total = (b * bc) + (w * wc)
@@ -18,6 +18,8 @@ function taumBday(b, w, bc, wc, z) {
 
 
 (()=>{
-    let b = 3 , w = 6, bc = 9, wc = 1, z = 1;
+    let b = 742407782  , w = 90529439, bc = 847666641, wc = 8651519, z =   821801924;
     console.log('taumBday(b, w, bc, wc, z):', taumBday(b, w, bc, wc, z))
 })()
+
+
