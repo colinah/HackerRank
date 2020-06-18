f6e751abcd2572253392d41d4594bdf032aeeb5a
colinah <halecolin1@gmail.com>
2020-06-18 10:27:39 -0600

    Adding test case to checkmagazine function

---
+++ checkMagazine.js
@@ -0,0 +1,9 @@
+function checkMagazine (magazine, note) {
+
+}
+
+(()=>{
+    const magazine = [ 'give', 'me', 'one', 'grand', 'today', 'night' ];
+    const note = [ 'give', 'one', 'grand', 'today' ]
+    checkMagazine (magazine, note)
+})()
