c6428ff037a22020817d7a003211666202d67bdc
colinah <halecolin1@gmail.com>
2020-06-18 10:48:39 -0600

    Adding return statement

--- checkMagazine.js
+++ checkMagazine.js
@@ -1,5 +1,16 @@
 function checkMagazine (magazine, note) {
+    // Need to go through each item of note and see if magazine contains an exact match and duplicate words if nessecary.
 
+
+
+
+
+    //Print Yes or no if note can be written with magazine
+    if(checker){
+        console.log('YES');
+    } else {
+        console.log('NO')
+    }
 }
 
 (()=>{
