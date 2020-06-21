c140f20b7471967e3660b52c448514310055b83b
colinah <halecolin1@gmail.com>
2020-06-21 16:55:04 -0600

    Adding nested loops

--- countTriplets.js
+++ countTriplets.js
@@ -1,4 +1,21 @@
+// Create function to return the total number of "triplets" in an arr.
+// Triples are an arr [i,j,k] where i < j < k
+//Loop through arr checking for triplets, nested loop with break two check 
 function countTriplets(arr, r) {
+    let count = 0;
+    l1: for(let i = 0; i < arr.length - 3;i++){
+        let triplet = []
+        let condition = false
+        l2: for(let j = i; j < arr.length - 3; j++) {
+            
+        }
+    }
 
 
 }
+
+(()=>{
+    let arr = [1,4,4,16,64];
+    let r = 0;
+    console.log('coutTriplets: ', countTriplets(arr,r))
+})
